49a255595fc6766b8c4efcfab9e95638617e9d0b
iPhysX <jasonwalsh@blueyonder.co.uk>
2011-08-26 13:59:10 +0100

    pussssh

--- FlowerPower/src/troll/coding/FlowerPower/EListener.java
+++ FlowerPower/src/troll/coding/FlowerPower/EListener.java
@@ -1,43 +1,106 @@
 package troll.coding.FlowerPower;
 
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityListener;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 
-public class EListener extends EntityListener
-	{
-		@SuppressWarnings("unused")
-		private FlowerPower plugin;
-
-		public EListener(FlowerPower plugin) {
-			this.plugin = plugin;
-
-		}
-
-		@Override
-		public void onExplosionPrime(ExplosionPrimeEvent event) {
-			Player p = (Player) event.getEntity();
-			Entity ExplosionCause = event.getEntity();
-
-			if (event.isCancelled() == true) {
-
-				return;
-
-			} else {
-
-				if (ExplosionCause instanceof Creeper) {
-
-					return;
-
-				} else {
-
-					SpawnFlowers.spawnFlowers(p, 10, p.getWorld(), p.getLocation());
-					event.setCancelled(true);
-				}
-
-			}
-		}
+public class EListener extends EntityListener{
+	@SuppressWarnings("unused")
+	private FlowerPower plugin;
+	
+	public EListener(FlowerPower plugin) {
+		this.plugin = plugin;
 
 	}
+
+	
+	@Override
+	public void onExplosionPrime(ExplosionPrimeEvent event){		
+			
+
+
+	
+		int y = 0;
+		
+		Entity ExplosionCause = event.getEntity();
+		
+		Location loc = ExplosionCause.getLocation();
+		
+		World world = ExplosionCause.getWorld();
+		
+		Entity p = null;
+		int up = 0;
+		
+		List<Entity> entt = ExplosionCause.getNearbyEntities(5, 5, 5);
+		
+		
+		
+		while (y < 25){
+		
+			p = entt.get(y);
+			
+			if (p instanceof Player){
+				
+				y = 26;
+			}else{
+				
+				y++;
+			}
+		}
+		
+		
+		
+		if (event.isCancelled() == true){
+			
+			return;
+			
+		}else{
+		
+		
+		if (ExplosionCause instanceof Creeper){
+			
+		return;
+			
+		}else{
+			
+			event.setCancelled(true); 
+			
+			Entity squid = world.spawnCreature(loc, CreatureType.SQUID);
+			
+			
+			p.setPassenger(squid);
+			SpawnFlowers.spawnFlowers((Player) event.getEntity(), 10, p.getWorld(), p.getLocation());
+			
+			Location locc = p.getLocation();
+			
+			
+			
+			for (up = 0; up > 10; up++) {
+				
+			locc.setY(y+2);
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		}
+		
+		
+	}
+	}
+	
+
